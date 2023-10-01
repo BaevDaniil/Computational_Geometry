047613f9e1b26c6ffd9289b0bd65ec71fbde8d63
Daniil Baev <baev.daniil.2002@gmail.com>
2023-10-01 13:17:11 +0300

    Fix error and not adding duplicate vertices

--- polygon.py
+++ polygon.py
@@ -30,12 +30,13 @@ class polygon:
         q1 = poly.vertexes[j - 1]
         result = polygon([])
         inside = ""
-        while iteration <= 2 * (len(self.vertexes) + len(poly.vertexes)):
+        while iteration < 2 * (len(self.vertexes) + len(poly.vertexes)):
             if (utils.isSegmentIntersecting(p1, p2, q1, q2) and (q2[0]-q1[0]) * (p2[1]-p1[1]) - (p2[0]-p1[0]) * (q2[1]-q1[1]) != 0):
                 newVertex = utils.findSegmentIntersecting(p1, p2, q1, q2)
-                if (len(result.vertexes) > 2 and newVertex == result.vertexes[0]):
+                if (len(result.vertexes) != 0 and newVertex == result.vertexes[0] and result.vertexes[-1] != result.vertexes[0]):
                     return result
-                result.vertexes.append(newVertex)
+                if (len(result.vertexes) == 0 or newVertex != result.vertexes[-1]):
+                    result.vertexes.append(newVertex)
                 if (utils.doubleSabc(q1, q2, p2) >= 0):
                     inside = "p"
                 else:
@@ -47,14 +48,16 @@ class polygon:
                     if (j == len(poly.vertexes)):
                         j = 0
                     if inside == "q":
-                        result.vertexes.append(q2)
+                        if (len(result.vertexes) == 0 or result.vertexes[-1] != q2):
+                            result.vertexes.append(q2)
                 else:
                     #advance p
                     i += 1
                     if (i == len(self.vertexes)):
                         i = 0                  
                     if inside == "p":
-                        result.vertexes.append(p2)
+                        if (len(result.vertexes) == 0 or result.vertexes[-1] != p2):
+                            result.vertexes.append(p2)
             else:
                 if (utils.doubleSabc(p1, p2, q2) >= 0):
                     #advance p
@@ -62,14 +65,16 @@ class polygon:
                     if (i == len(self.vertexes)):
                         i = 0
                     if inside == "p":
-                        result.vertexes.append(p2)                
+                        if (len(result.vertexes) == 0 or result.vertexes[-1] != p2):
+                            result.vertexes.append(p2)                
                 else:
                     #advance q
                     j += 1
                     if (j == len(poly.vertexes)):
                         j = 0
                     if inside == "q":
-                        result.vertexes.append(q2)
+                        if (len(result.vertexes) == 0 or result.vertexes[-1] != q2):
+                            result.vertexes.append(q2)
             p2 = self.vertexes[i]
             p1 = self.vertexes[i - 1]
             q2 = poly.vertexes[j]
